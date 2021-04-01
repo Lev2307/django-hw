8a16604aa1dd1882973163be0c1e567b97825268
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2021-04-01 18:09:39 +0300

    Add files via upload

--- first_app/news/forms.py
+++ first_app/news/forms.py
@@ -18,11 +18,15 @@ class NewsModelForm(forms.ModelForm):
         data = self.cleaned_data.get('article')
         if len(data) < 5:
             raise forms.ValidationError('Article isn`t long enough')
+        elif len(data) > 20:
+            raise forms.ValidationError('Article is too long')
         return data
     
     def clean_body(self):
         data = self.cleaned_data.get('body')
         if len(data) < 15:
             raise forms.ValidationError('Body isn`t long enough')
+        elif len(data) > 45:
+            raise forms.ValidationError('Body is too long')
         return data
     
